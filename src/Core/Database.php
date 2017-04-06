4c7f965e4c8e41d968a0e112798150462f7ea2a2
Petr Karmashev <smonkl@bk.ru>
2017-04-06 21:36:52 +0300

    Database abstraction class

---
+++ src/Core/Database.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Petr\Comments\Core;
+
+/**
+ * Database abstraction
+ * @author Vehsamrak
+ */
+class Database
+{
+
+    /** @var \PDO */
+    private $connection;
+
+    /** @inheritdoc */
+    public function __construct()
+    {
+        $dsn = sprintf(
+            '%s:host=%s;dbname=%s',
+            Config::get('database_type'),
+            Config::get('database_host'),
+            Config::get('database_name')
+        );
+        $this->connection = new \PDO(
+            $dsn,
+            Config::get('database_user'),
+            Config::get('database_password')
+        );
+    }
+
+    /**
+     * @return \PDO
+     */
+    public function getConnection(): \PDO
+    {
+        return $this->connection;
+    }
+}
