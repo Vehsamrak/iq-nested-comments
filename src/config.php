4dc38a49d346c5d4ef45fd066a5beec7244a0f26
Petr Karmashev <smonkl@bk.ru>
2017-04-06 23:43:39 +0300

    Database configuration

--- src/config.php
+++ src/config.php
@@ -1,3 +1,9 @@
 <?php
 
-return [];
+return [
+    'database_type'     => 'mysql',
+    'database_host'     => '127.0.0.1',
+    'database_name'     => 'iq_option_comments',
+    'database_user'     => 'root',
+    'database_password' => '',
+];
